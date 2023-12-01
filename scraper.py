832ef9fa286eb9b52c4bf5634a072ca72683f43f
Rascal <t.arai14916@gmail.com>
2023-12-01 16:19:52 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 import requests
 import os
 from dotenv import load_dotenv
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
